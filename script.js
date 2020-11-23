6794dea02385c959f46589db06144484a242680c
Josue Narro <josue.narro.a@gmail.com>
2020-11-23 13:19:12 -0500

    Create setAttributes helper to enhance code

--- script.js
+++ script.js
@@ -8,19 +8,30 @@ const count = 10;
 const apiKey = "duLRyqRrXqr7n5FxwmgTB3vrsND4AVRspwa7DK2wt_c";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to set attributes
+function setAttributes(element, attributes) {
+  for (let key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create elements for links & photos, add to DOM
 function displayPhotos() {
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
 
     // Create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
     // Put <img> inside <a>, then put both inside imageContainer
     item.append(img);
