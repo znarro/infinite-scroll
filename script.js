39314498b29cd1371750ee92bd019b6aa9c22f6f
Josue Narro <josue.narro.a@gmail.com>
2020-11-23 12:45:57 -0500

    Create getPhotos

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = "duLRyqRrXqr7n5FxwmgTB3vrsND4AVRspwa7DK2wt_c";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch errors
+  }
+}
+
+// On load
+getPhotos();
